1bf6b07110e9b2eba870acf836e8589bf7797b2c
kahhorov <sherjahonkahhorov>
2025-08-23 17:38:10 +0500

    edits

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Masters from "../Page/Masters";
 
-export default () => {
+export default ({ cartAccordions }) => {
+  useEffect(() => {
+    console.log("Main sahifaga kelgan cartAccordions =", cartAccordions);
+  }, [cartAccordions]);
   return (
     <div>
       <Masters />
